c7ff53172b76629f4d95822cbcd48977921d071f
irfan wahid <muhammad.irfan.tif20@gmail.com>
2025-01-13 17:50:44 +0700

    add resume

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Component } from 'react';
 import $ from 'jquery';
 import Header from './Components/Header';
 import About from './Components/About';
+import Contact from './Components/Contact';
 
 class App extends Component {
   constructor(props){
@@ -43,6 +44,7 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
+        <Contact data={this.state.resumeData.main}/>
       </div>
     );
   }
